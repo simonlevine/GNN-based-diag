497ca532c72a3b195918bb095a673d04d7f268bb
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-17 11:53:27 -0800

    Update main.py

--- main.py
+++ main.py
@@ -67,10 +67,10 @@ def train(model, train_loader, optimizer, criterion):
 
     for data in train_loader:  # Iterate in batches over the training dataset.
 
-        data.to(DEVICE)
+        # data.to(DEVICE)
 
-        out = model(data.x, data.edge_index, data.batch)  # Perform a single forward pass.
-        loss = criterion(out, data.y)  # Compute the loss.
+        out = model(data['x'], data['edge_index'],data['batch'])  # Perform a single forward pass.
+        loss = criterion(out, data['y'])  # Compute the loss.
         loss.backward()  # Derive gradients.
         optimizer.step()  # Update parameters based on gradients.
         optimizer.zero_grad()  # Clear gradients.
@@ -79,11 +79,11 @@ def test(model,test_loader):
      model.eval()
      correct = 0
      for data in test_loader:  # Iterate in batches over the training/test dataset.
-        data.to(DEVICE)
+        # data.to(DEVICE)
         
-        out = model(data.x, data.edge_index,data.batch)  
+        out = model(data['x'], data['edge_index'],data['batch'])  
         pred = out.argmax(dim=1)  # Use the class with highest probability.
-        correct += int((pred == data.y).sum())  # Check against ground-truth labels.
+        correct += int((pred == data['y']).sum())  # Check against ground-truth labels.
      return correct / len(test_loader.dataset)  # Derive ratio of correct predictions.
 
 
